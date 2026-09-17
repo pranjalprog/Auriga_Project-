a911045923c14dda4986280e2f86ec14e0002f39
Pranjal Jain <pranjaljain19022005@gmail.com>
2026-09-17 09:42:46 +0000

    Setup: Spring Boot skeleton + DB schema

---
+++ backend/src/main/java/com/auriga/clinic/AurigaClinicApplication.java
@@ -0,0 +1,13 @@
+package com.auriga.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurigaClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AurigaClinicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/auriga/clinic/AurigaClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auriga.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurigaClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
